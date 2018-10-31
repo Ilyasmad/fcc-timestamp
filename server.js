8aec9c2d20f6dec4f696dda85d16dacb1170d609
Ahmad Ilyas Roose <43195364+Ilyasmad@users.noreply.github.com>
2018-10-31 18:50:57 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,55 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint...
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get('/api/timestamp', (req, res) => {
+  let time = new Date();
+  let unix = time.getTime();
+  let utc = time.toUTCString();
+
+  res.json({unix: unix, utc: utc})
+});
+
+ app.get('/api/timestamp/:date_string', (req, res) => {
+  let time = req.params.date_string;
+
+  if (isNaN(time)) {
+    var unix = new Date(time).getTime();
+    var utc = new Date(time).toUTCString();
+  }
+  else {
+    var unix = parseInt(time*1000);
+    var utc = new Date(time*1000).toUTCString();
+  };
+
+  res.json({unix: unix, utc: utc})
+});
+
+app.get('/echo/:word', (req, res) => res.json({echo: req.params.word}));
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
